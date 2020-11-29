bb826e4bfece0023646e1721f2731f395e88fdf6
Harsh Panjwani <panjwani28.harsh@gmail.com>
2020-11-29 19:07:50 +0530

    DB connection

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
-//const connectDB =  require('./config/db');
+const connectDB =  require('./config/db');
 
 const app = express()
 
 //ConnectDB
-//connectDB();
+connectDB();
 
 //Init Middleware
 //app.use(express.json({extended: false}));

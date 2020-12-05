89caf92914c1a9b8598170df53390501e5eabeec
Harsh Panjwani <panjwani28.harsh@gmail.com>
2020-12-05 23:48:33 +0530

    port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json({extended: false}));
 
 app.get('/', (req,res) => res.send('API Runing')); 
 
-const PORT =  process.env.PORT || 3000;
+const PORT =  process.env.PORT || 5000;
 
 //route
 app.use('/users' , require("./routes/users"));

996db274e4f99f0190450aad4aeba19946404dc1
Harsh Panjwani <panjwani28.harsh@gmail.com>
2020-12-11 12:38:32 +0530

    Yahoo setu[

--- config/keys.js
+++ config/keys.js
@@ -4,9 +4,9 @@
 
 module.exports = {
 	mailer: {
-		service: 'yahoo', //service
+		service: 'Yahoo', //service
 		user: 'lets_ba@yahoo.com',  //username of mail service
-		pass: 'Dinesh@123', //password of mail service
+		pass: 'ijbfezarfbqpbrgk', //password of mail service
 		reporting_email: "lets_ba@yahoo.com" //reply mail
     }
 }
